d302dcbc5d6413ba7059c2aa89b6ec4689564770
tpellard <thomas.pellard@gmail.com>
2023-05-03 23:52:08 +0200

    Update FiguresTreeModel.R

--- FiguresTreeModel.R
+++ FiguresTreeModel.R
@@ -6,132 +6,219 @@ library(ape)
 library(phangorn)
 library(ggplot2)
 library(cowplot)
+library(ggthemes)
+library(knitr)
+
+# Import data -------------------------------------------------------------
+
+tt <- read.nexus("SinoTibetanSubset.nex")
 
 
-tt = read.nexus("SinoTibetanSubset.nex")
+# Consensus tree ----------------------------------------------------------
 
 # Consensus topology
-cons = consensus(tt, p=.5, rooted=T)
-cons$node.label = round(cons$node.label * 100, 0)
-plot.phylo(cons, show.node.label = T) 
+cons <- consensus(tt, p = .5, rooted = T)
+cons$node.label <- round(cons$node.label * 100, 0)
 
 # Consensus topology + branch lengths. This might take a couple of minutes
-ct = consensus.edges(tt, consensus.tree = cons, rooted=T)
-plot(ct, show.node.label=T) 
+ct <- consensus.edges(tt, consensus.tree = cons, rooted = T)
+ct$root.edge <- .15
+
+pdf("fig_consensus.pdf", pointsize=10, width = 6, height = 4, family = "URWPalladio")
+plot(ct, show.node.label=FALSE, root.edge = TRUE, no.margin = TRUE, font = 1, label.offset = .05)
+nodelabels(ct$node.label, frame="none", adj = c(-0.2,.5), cex = .85)
+dev.off()
+embedFonts("fig_consensus.pdf")
+plot_crop("fig_consensus.pdf")
 
 
+# Densitree ---------------------------------------------------------------
 
-plot.phylo(ct, show.node.label = T, main ="Consensus tree")
-
-
-
-# Maximum clade credibility tree
-mcc = mcc(tt)
-mcc$node.label = round(mcc$node.label, 2)
-mcc$node.label[getRoot(mcc)] = NA
-
-plot(mcc, show.node.label = T, main="MCC")
+pdf("fig_densitree.pdf", pointsize=10, width = 6.5, height = 7, family = "URWPalladio")
+densiTree(tt, consensus = cons, alpha=.005, font = 1, label.offset = .01, cex=.9, scale.bar = TRUE)
+title(xlab="Time (millenia)")
+dev.off()
+embedFonts("fig_densitree.pdf")
+knitr::plot_crop("fig_densitree.pdf")
 
 
-# Densitree
-densiTree(tt, alpha=.005, consensus = cons, main="Densitree")
+# Root age plot -----------------------------------------------------------
 
-
-
-
-# posterior probability of a specific subtree
-subtree_support = function(tips, trees = tt){
-  sapply(trees, is.monophyletic, tips=tips)
+# Posterior probability of a specific subtree
+subtree_support <- function(tips, trees = tt) {
+  sapply(trees, is.monophyletic, tips = tips)
 }
-mean(subtree_support(c("Chepang","Hayu","Thulung","Bokar_Tani","Yidu","Tshangla")))
+mean(subtree_support(c("Chepang", "Hayu", "Thulung", "Bokar_Tani", "Yidu", "Tshangla")))
 # The 6 leaves listed above form a subtree with posterior probability 0.41
-
-
 
 # Creating the plot for the root age depending on the outgroup
 # Lists of outgroups
-find.outgroup = function(tree, nl = length(tree$tip.label)){
-  r = getRoot(tree)
-  cr = tree$edge[tree$edge[, 1] == r , 2]
-  s1 = getDescendants(tree, cr[1])
-  s2 = getDescendants(tree, cr[2])
-  
-  s1 = s1[s1<=nl]
-  s2 = s2[s2<=nl]
-  
+find.outgroup <- function(tree, nl = length(tree$tip.label)) {
+  r <- getRoot(tree)
+  cr <- tree$edge[tree$edge[, 1] == r, 2]
+  s1 <- getDescendants(tree, cr[1])
+  s2 <- getDescendants(tree, cr[2])
+  s1 <- s1[s1 <= nl]
+  s2 <- s2[s2 <= nl]
   # Ensure that s1 is the smallest
-  if(length(s1) > length(s2)) s1 = s2
+  if (length(s1) > length(s2)) s1 <- s2
   return(paste(sort(tree$tip.label[s1]), collapse = " "))
 }
 
-outgroup = sapply(tt, find.outgroup, nl=22)
-head(sort(table(outgroup), dec=T), 3)
+outgroup <- sapply(tt, find.outgroup, nl = 22)
+head(sort(table(outgroup), dec = T), 3)
 
-outgroup[outgroup == "Beijing_Chinese Guangzhou_Chinese Jieyang_Chinese Xingning_Chinese"] = "Chinese"
-outgroup[outgroup == "Beijing_Chinese Guangzhou_Chinese Jieyang_Chinese Jingpho Rabha Xingning_Chinese"] = "Chinese + Sal"
-outgroup[outgroup == "Bokar_Tani Yidu"] = "Tani-Yidu"
+outgroup[outgroup == "Beijing_Chinese Guangzhou_Chinese Jieyang_Chinese Xingning_Chinese"] <- "Chinese"
+outgroup[outgroup == "Beijing_Chinese Guangzhou_Chinese Jieyang_Chinese Jingpho Rabha Xingning_Chinese"] <- "Chinese + Sal"
+outgroup[outgroup == "Bokar_Tani Yidu"] <- "Tani-Yidu"
 
-root.age = function(tree){ node.depth.edgelength(tree)[1]}
-ra = sapply(tt, root.age)
-aged = data.frame(age = ra * 1000, outgroup = outgroup)
-keep = outgroup %in% c("Chinese", "Tani-Yidu", "Chinese + Sal")
+root.age <- function(tree) {
+  node.depth.edgelength(tree)[1]
+}
+ra <- sapply(tt, root.age)
+aged <- data.frame(age = ra * 1000, outgroup = outgroup)
+keep <- outgroup %in% c("Chinese", "Tani-Yidu", "Chinese + Sal")
 
-aged2 = rbind(aged[keep, ], data.frame(age = ra*1000, outgroup="all"))
+aged2 <- rbind(aged[keep, ], data.frame(age = ra * 1000, outgroup = "all"))
 
-
-p.age.group = ggplot(aged[keep,], aes(x=age)) + 
+p.age.group <- ggplot(aged[keep, ], aes(x = age)) +
   xlim(0, 15000) +
-  geom_density(aes(group=outgroup, colour=outgroup, fill=outgroup), alpha=.3)
+  geom_density(aes(group = outgroup, colour = outgroup, fill = outgroup), alpha = .3) + 
+  scale_fill_few() + scale_color_few() + 
+  theme_minimal() +
+  theme(legend.position = c(1,.5), legend.justification = c(1,.5))
 
-col="purple"
-p.age = ggplot(aged, aes(x=age)) +
+col <- "purple"
+p.age <- ggplot(aged, aes(x = age)) +
   xlim(0, 15000) +
-  geom_density(fill=col, colour=col, alpha=.3)
-           
+  geom_density(fill = col, colour = col, alpha = .3) + 
+  theme_minimal()
+
+plot_grid(p.age, p.age.group, align = "v", axis = "rblt", ncol = 1)
+
+options(scipen = 999)
+pdf("fig_ageboth.pdf", pointsize=10, width = 5, height = 5/1.6*2, family = "URWPalladio")
 plot_grid(p.age, p.age.group, align="v", axis="rblt", ncol=1)
+dev.off()
+embedFonts("fig_ageboth.pdf")
+knitr::plot_crop("fig_ageboth.pdf")
 
+library(ggridges)
+ggplot(aged2, aes(x = age, y = outgroup, group = outgroup, height = stat(density))) +
+  geom_density_ridges(fill = few_pal("Light")(2)[1], color = "gray30") +
+  theme_minimal() +
+  xlab("age (years)") +
+  theme(axis.text.y.left = element_text(size = 11))
 
 ###### MRCA ages
 
-mrca.age = function(tree, tips){
-  max(dist.nodes(tree)[tips, tips])/2
+mrca.age <- function(tree, tips) {
+  max(dist.nodes(tree)[tips, tips]) / 2
 }
 
-mrca.age.names = function(tree, tipnames){
+mrca.age.names <- function(tree, tipnames) {
   max(cophenetic(tree)[tipnames, tipnames]) / 2
 }
 
-mrca.summary = function(d, l){
-  nt = length(d)
-  res = rep(NA, nt)
-  for(i in 1:nt){
-    if(is.numeric(l)){
-      res[i] = mrca.age(d[[i]], l)
-    }
-    else{
-      res[i] = mrca.age.names(d[[i]], l)
+mrca.summary <- function(d, l) {
+  nt <- length(d)
+  res <- rep(NA, nt)
+  for (i in 1:nt) {
+    if (is.numeric(l)) {
+      res[i] <- mrca.age(d[[i]], l)
+    } else {
+      res[i] <- mrca.age.names(d[[i]], l)
     }
   }
-  
+
   return(res)
 }
 
 # Plot of MRCA age for the following languages: Jingpho, Rabha, 4 Chinese dialects
-ll = tt[[1]]$tip.label
-l = c(7, 12, 13:16) # indices of the languages of interest in vector ll
-ages = mrca.summary(tt, l)
-lab = sapply(tt, is.monophyletic, tips=l)
-df = data.frame(age=ages, monophyletic=lab)
+ll <- tt[[1]]$tip.label
+l <- c(7, 12, 13:16) # indices of the languages of interest in vector ll
+ages <- mrca.summary(tt, l)
+lab <- sapply(tt, is.monophyletic, tips = l)
+df <- data.frame(age = ages, monophyletic = lab)
 
-p.age.topo.group = ggplot(df, aes(x=age)) + 
-  geom_density(aes(group=monophyletic, colour=monophyletic, fill=monophyletic), alpha=.3) 
+p.age.topo.group <- ggplot(df, aes(x = age)) +
+  geom_density(aes(group = monophyletic, colour = monophyletic, fill = monophyletic), alpha = .3)
 
-p.age.topo.all = ggplot(df, aes(x=age)) + 
-  geom_density(fill=col, colour=col, alpha=.3)
+p.age.topo.all <- ggplot(df, aes(x = age)) +
+  geom_density(fill = col, colour = col, alpha = .3)
 
-title = ggdraw() + draw_label("Age of MRCA" , fontface='bold')
+title <- ggdraw() + draw_label("Age of MRCA", fontface = "bold")
 
-plot_grid(title, p.age.topo.all, p.age.topo.group, align="v", axis="rblt", ncol=1, rel_heights=c(0.2, 1, 1))
+plot_grid(title, p.age.topo.all, p.age.topo.group, align = "v", axis = "rblt", ncol = 1, rel_heights = c(0.2, 1, 1))
+
+library(tidyverse)
+df %>% 
+  mutate(group = ifelse(monophyletic == TRUE, "monophyletic", "paraphyletic")) %>% 
+  bind_rows(mutate(df, group = "all")) %>% 
+  ggplot(aes(x = age, y = group, height = stat(density))) +
+  geom_density_ridges(fill = few_pal("Light")(2)[1], color = "gray30") +
+  theme_minimal() +
+  xlab("age (years)") +
+  theme(axis.text.y.left = element_text(size = 11))
 
 
+# Cross-validation of dates -----------------------------------------------
+library(HDInterval)
+
+# True age intervals
+true.tibetan = c(1000, 1200)
+true.burmish = c(700, 900)
+true.commonchinese = c(2000, 2200)
+true.sinitic = c(2400, 2600)
+
+reconstructed.age = function(file, lang){
+  t = read.nexus(file)
+  
+  #remove 20% for burn-in
+  nt = length(t)
+  t = t[(nt/2) : nt]
+  
+  age = rep(NA, length(t))
+  for(i in 1:length(t)){
+    tt = t[[i]]
+    
+    if(length(lang) == 1){
+      idx = which(tt$tip.label == lang)
+    } else {
+      idx = getMRCA(tt, lang)
+    }
+    
+    depths = node.depth.edgelength(tt)
+    age[i] = max(depths) - depths[idx]
+  }
+  return(hdi(age))
+}
+
+
+recage.burmish = reconstructed.age("xval/Burmese.nex", 
+                                   c("BurmishOldBurmese", "BurmishRangoon"))
+
+recage.commonchinese = reconstructed.age("xval/CommonChinese.nex", 
+                                         c("SiniticBeijing", "SiniticGuangzhou", 
+                                           "SiniticXingning", "SiniticLonggang",
+                                           "SiniticJieyang", "SiniticChaozhou" ))
+recage.sinitic = reconstructed.age("xval/Chinese.nex", 
+                                   c("SiniticOldChinese" ))
+recage.tibetan = reconstructed.age("xval/Tibetan.nex", 
+                                   c("TibetanOldTibetan"))
+
+
+df = as.data.frame(rbind(true.burmish, true.commonchinese, true.sinitic, true.tibetan,
+                         recage.burmish, recage.commonchinese, recage.sinitic, recage.tibetan))
+df$type = rep(c("Constraint", "Reconstructed"), each=4)
+df$clade = rep(c("Burmish", "Common Chinese", "Sinitic", "Tibetan"), 2)
+
+ggplot(df, aes(x=clade, y=(lower+upper)/2, ymin=lower, ymax=upper)) +
+  geom_linerange(aes(color=type), position=position_dodge(width=c(0.2)), size=1) +
+  ylab("Years BP") +
+  scale_color_few() +
+  # coord_equal() +
+  theme_minimal() +
+  theme(legend.position = "top", aspect.ratio = .6)
+  # theme(legend.position = c(1,.5), legend.justification = c(1,.5))
 
